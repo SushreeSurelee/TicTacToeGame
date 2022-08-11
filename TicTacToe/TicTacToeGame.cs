ea1cfcad5129629f4375f6d9dcfcad52475b1201
“Sushree” <sushreesureleesahu98@gmail.com>
2022-08-11 08:55:06 +0530

    [Sushree | added fuctionality to create a empty tic tac toe game]

---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        public static void TicTacToe()
+        {
+            char[] game = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            for (int i=1;i<10;i++)
+            {
+                game[i] = ' ';
+            }
+        }
+    }
+}
